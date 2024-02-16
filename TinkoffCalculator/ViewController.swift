6163a8e5263d405f86ab6f5fe5f2b28d10b100f2
Ankor <ankor_socialki@mail.ru>
2024-02-16 11:52:32 +0300

    -add methods

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -7,18 +7,131 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CalculationError: Error {
+    case dividedByZero
+}
 
+enum Operation: String {
+    case add = "+"
+    case substract = "-"
+    case multiply = "x"
+    case divide = "/"
+
+    func calculate(_ number1: Double, _ number2: Double) throws -> Double {
+        switch self {
+        case .add:
+            return number1 + number2
+        case .substract:
+            return number1 - number2
+        case .multiply:
+            return number1 * number2
+        case .divide:
+            if number2 == 0 {
+                throw CalculationError.dividedByZero
+            }
+            return number1 / number2
+        }
+    }
+}
+
+enum CalculationHistoryItem {
+    case number(Double)
+    case operation(Operation)
+}
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    var calculationHistory: [CalculationHistoryItem] = []
+    
+    lazy var numberFormater: NumberFormatter = {
+        let numberFormater = NumberFormatter()
+        
+        numberFormater.usesGroupingSeparator = false
+        numberFormater.locale = Locale(identifier: "ru_RU")
+        numberFormater.numberStyle = .decimal
+        
+        return numberFormater
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        resetLabelText()
     }
     
+    //MARK: Methods
+    @IBAction func calculateButtonPressed() {
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormater.number(from: labelText)?.doubleValue
+        else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        
+        do {
+            let result = try calculate()
+            
+            label.text = numberFormater.string(from: NSNumber(value: result))
+        } catch {
+            label.text = "Error"
+        }
+        calculationHistory.removeAll()
+        
+    }
+    @IBAction func clearButtonPressed() {
+        calculationHistory.removeAll()
+        
+        resetLabelText()
+    }
     @IBAction func buttonPressed(_ sender: UIButton) {
         guard let buttonText = sender.currentTitle else { return }
         
-        print(buttonText)
+        if buttonText == "," && label.text?.contains(",") == true {
+            return
+        }
+        
+        if label.text == "0" {
+            label.text = buttonText
+        } else {
+            label.text?.append(buttonText)
+        }
     }
-
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        guard 
+            let buttonText = sender.currentTitle,
+            let buttonOperation = Operation(rawValue: buttonText)
+        else { return }
+        
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormater.number(from: labelText)?.doubleValue
+        else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        calculationHistory.append(.operation(buttonOperation))
+        
+        resetLabelText()
+    }
+    
+    func resetLabelText() {
+        label.text = "0"
+    }
+    
+    func calculate() throws -> Double {
+        guard case .number(let firstNumber) = calculationHistory[0] else { return 0 }
+        var currentResult = firstNumber
+        
+        for index in stride(from: 1, to: calculationHistory.count - 1, by: 2) {
+            guard
+                case .operation(let operation) = calculationHistory[index],
+                case .number(let number) = calculationHistory[index + 1]
+            else { break }
+            
+            currentResult = try operation.calculate(currentResult, number)
+        }
+        return currentResult
+    }
 }
 
